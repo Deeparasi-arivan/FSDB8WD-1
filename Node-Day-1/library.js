42efff7ee60af1867763d2953ad9289b00553a43
Manikandan Anbalagan <manikandana121290@gmail.com>
2024-11-28 20:27:19 +0530

    changes

--- Node-Day-1/library.js
+++ Node-Day-1/library.js
@@ -1,8 +1,6 @@
 const fs = require("fs"); //fs is a module of node
 const path = require("path"); //path is a module of node
 
-const chalk = require("chalk"); //chalk is a module of node
-
 // Folder for storing our new books
 const libraryPath = path.join(__dirname, "library"); //__dirname is a global variable in node
 
