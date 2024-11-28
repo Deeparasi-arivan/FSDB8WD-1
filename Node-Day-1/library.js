c8a9e8234dc05170ee9092d484eb0ec3a0426def
Manikandan Anbalagan <manikandana121290@gmail.com>
2024-11-28 20:26:10 +0530

    node-day-1

---
+++ Node-Day-1/library.js
@@ -0,0 +1,28 @@
+const fs = require("fs"); //fs is a module of node
+const path = require("path"); //path is a module of node
+
+const chalk = require("chalk"); //chalk is a module of node
+
+// Folder for storing our new books
+const libraryPath = path.join(__dirname, "library"); //__dirname is a global variable in node
+
+// Create the folder if it doesn't exist
+if (fs.existsSync(libraryPath)) {
+  console.log("Library already exists");
+} else {
+  fs.mkdirSync(libraryPath);
+  console.log("Library created");
+}
+
+function addBook(bookName, content) {
+  const bookPath = path.join(libraryPath, `${bookName}.txt`); //path.join is a function of path module
+  fs.writeFileSync(bookPath, content, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Book added");
+    }
+  });
+}
+
+addBook("book1", "This is the content of book1");
